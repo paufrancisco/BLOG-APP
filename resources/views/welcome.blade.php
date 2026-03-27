6323b415a0973f7296ea9e95637d4daa683ad2a7
paufrancisco <francisco.paulo.pf@gmail.com>
2026-03-27 13:04:50 +0800

    g

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Blog App</title>
-        @vite('resources/js/app.js')
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body style="margin:0; background:#faf8f3;">
         <div id="app"></div>
